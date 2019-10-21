e3d4e9d3863b82ecea217e7a2b964c1ccff6ec34
Pontsho MOGWERE <pmogwere@c5r5s6.wethinkcode.co.za>
2019-10-21 13:02:31 +0200

    reset password code

--- userauth.php
+++ userauth.php
@@ -54,8 +54,27 @@
         /* send email to the user if the $username||$email provided is correct */
         public function passwordreset($username)
         {
+            $selec = bin2hex(random_bytes(8));
+            $token = random_bytes(32);
 
-        }
+            $url = "www.localhost/create_new_password.php?selec=".$selec."&validator=".bin2hex($token);
+            $exp = date("U") + 1800;
+            $user = $_POST['email'];
+            $to = $username;
+            $subject = "Reset your passord for camagru";
+
+            $message =  "We revieved a password reset request. The link to reset your password";
+            $message .= '<a href="'. $url . '">'. $url.'</a>';
+
+            $headers = "From: camagru <camagru@gmail.com>\r\n";
+            $headers .= "Reply-To: camagru@gmail.com\r\n";
+            $headers .= "Content-type: text/html\r\n";
+
+            mail($to,$subject, $message,$headers);
+
+            header("location: #");
+
+            }
         /* disconnecting from the database */
         function __destruct(){
             $conns = NULL;
