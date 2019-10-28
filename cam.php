9e4d0ea73514648428d1bf8ba0d2ab67064a383e
Pontsho MOGWERE <pmogwere@c5r5s6.wethinkcode.co.za>
2019-10-28 14:52:06 +0200

    sss

--- cam.php
+++ cam.php
@@ -1,3 +1,20 @@
+<?php
+    $retrive = array();
+    foreach($_POST as $key => $value)
+        $retrive[$key] = $value;
+    if ($retrive["image"]) {
+        $reimage = explode(",", $retrive['image']);
+        $encodedData = str_replace(' ','+',$reimage[1]);
+        $decodedData = base64_decode($encodedData);
+        $fp = fopen("canvas.png", 'wb');
+        fwrite($fp, $decodedData);
+        fclose();
+        include_once('./picdb.php');
+        $as = new picdb();
+        $as->tempsave($retrive['image']);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,56 +22,106 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+ 
 </head>
 <body>
-    <div class="video-wrap">
-        <video id="video" playsinline autoplay></video>
+    <form actio="cam.php" method="post">
+
+        <div class="video-wrap">
+            <video id="video" playsinline autoplay></video>
+        </div>
+
+            <input type="hidden" value="" id="image" name="image">
+        <div class="controller">
+            <button id="snap" type="submit">Capture</button>
+            ballon<input type="radio" id="rad" name="ballon" value="ballon">
+            butterfly<input type="radio" id="rad" name="butterfly" value="butterfly">
+            vine<input type="radio" id="rad" name="vine" value="vine">
+            decor<input type="radio" id="rad" name="decor" palceholder="decor">
+        </div>
+
+        <canvas id="canvas" width="640" height="480"></canvas>
+    </form>
+    <div>
+        <?php
+            include_once('./picdb.php');
+            $arr = new picdb();
+            $display = $arr->getsave();
+            print_r($display);
+            // foreach($display as $key=>$value) 
+            //     echo $value; 
+        ?>
     </div>
 
-    <div class="controller">
-        <button id="snap">Capture</button>
+    <div>
+        <button id="save" style="display: none;">Save</button>
     </div>
-
-    <canvas id="canvas" width="640" height="480"></canvas>
-
+    <!-- <div id="imagediv">
+         <img src="" id="saveimage" style="float: left; border: 1px solid black; margin-left: 10px;">
+    </div> -->
     <script>
-        'use strict';
-        const video = document.getElementById('video');
-        const canvas = document.getElementById('canvas');
-        const snap = document.getElementById('snap');
-        const errorMsgElement = document.getElementById('span#ErrorMsg');
-        const constraints = {
-            audio: true,
-            video:{
-                width: 1280, height: 720
+            'use strict';
+            const video = document.getElementById('video');
+            const canvas = document.getElementById('canvas');
+            const snap = document.getElementById('snap');
+            const image = document.getElementById("image");
+            // const imagesave = document.getElementById("saveimage");
+            const errorMsgElement = document.getElementById('span#ErrorMsg');
+            const constraints = {
+                audio: true,
+                video:{
+                    width: 1280, height: 720
+                }
+            };
+            async function init()
+            {
+                try {
+                    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                    handleSuccess(stream);
+                } catch (e) {
+                    errorMsgElement.innerHTML = 'navigator.getUserMedia.error:${e.toString()}';
+                }
             }
-        };
-        async function init()
-        {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia(constraints);
-                handleSuccess(stream);
-            } catch (e) {
-                errorMsgElement.innerHTML = 'navigator.getUserMedia.error:${e.toString()}';
+            function handleSuccess(stream){
+                window.stream = stream;
+                video.srcObject = stream;
             }
-        }
-        function handleSuccess(stream){
-            window.stream = stream;
-            video.srcObject = stream;
-        }
-        init();
-        var context = canvas.getContext('2d');
-        snap.addEventListener("click", function(){
-            context.drawImage(video, 0, 0, 640, 480);
-            document.getElementById("video").style.display = "none";
-            document.getElementById("snap").style.display = "none";
-            let stream2 = video.srcObject;
-            let tracks = stream2.getTracks();
-            tracks.forEach(function(track){
-                track.stop();
-            }); 
-        });
-        
-    </script>
+            init();
+            var context = canvas.getContext('2d');
+            snap.addEventListener("click", function(){
+                context.drawImage(video, 0, 0, 640, 480);
+                // document.getElementById("video").style.display = "none";
+                // document.getElementById("snap").style.display = "none";
+                // let stream2 = video.srcObject;
+                // let tracks = stream2.getTracks();
+                // tracks.forEach(function(track){
+                //     track.stop();
+                // });
+                const dataURI = canvas.toDataURL();
+                //imagesave.src = dataURI;
+                image.setAttribute('value', dataURI);
+                // if (!document.getElementById('imagediv'))
+                // {
+                //     var imagedv = document.createElement("div");
+                //     // imagedv.setAttribute('id', 'imagediv');
+
+                // }else{
+                //     const imagedv = document.getElementById("imagediv");
+                // }
+
+                //     const dataURI = canvas.toDataURL();
+                //     imagesave.src = dataURI;
+            });
+
+            // const btndisplay = document.querySelector("#save");
+            // const imagesave = document.querySelector("#saveimage");
+
+            // btndisplay.addEventListener('click', function(){
+            //     const dataURI = canvas.toDataURI();
+            //     imagesave.src = dataURI;
+            // });
+            
+        </script>
+
 </body>
 </html>
