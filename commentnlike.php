85ed62d1a0b2aaa289340035e47121bfd1dd63e9
Pontsho MOGWERE <pmogwere@c5r5s6.wethinkcode.co.za>
2019-10-31 15:52:03 +0200

    can email when image comment on or liked

--- commentnlike.php
+++ commentnlike.php
@@ -7,16 +7,17 @@ class commentnlike{
         $this->conns = $conn;
     }
 
-    public function addcomment($comment, $useridown, $userid)
+    public function addcomment($comment, $useridown, $userid, $imagenu)
     {
         try{
-            $sql = 'INSERT INTO comments (userid, useridown, comment, timess)
-                    VALUES ( :userid, :useridown, :comment, :timess)';
+            $sql = 'INSERT INTO comments (userid, useridown, comment, timess, imagenu)
+                    VALUES ( :userid, :useridown, :comment, :timess, :imagenu)';
             $aa = $this->conns->prepare($sql);
             $aa->bindParam(':userid', $userid);
             $aa->bindParam(':useridown', $useridown);
             $aa->bindParam(':comment', $comment);
             $aa->bindParam(':timess', date("U"));
+            $aa->bindParam(':imagenu', $imagenu);
             $aa->execute();
         }catch (PDOException $e)
         {
@@ -24,20 +25,76 @@ class commentnlike{
         }
     }
 
-    public function addlike($useridown, $userid)
+    public function addlike($useridown, $userid, $imagenu)
     {
         try{
-            $sql = 'INSERT INTO likes (userid, useridown, timess)
-                    VALUES ( :userid, :useridown, :timess)';
+            $sql = 'INSERT INTO likes (userid, useridown, timess, imagenu)
+                    VALUES ( :userid, :useridown, :timess, :imagenu)';
             $aa = $this->conns->prepare($sql);
             $aa->bindParam(':userid', $userid);
             $aa->bindParam(':useridown', $useridown);
             $aa->bindParam(':timess', date("U"));
+            $aa->bindParam(':imagenu', $imagenu);
             $aa->execute();
         }catch (PDOException $e)
         {
             echo $sql . "<br>" . $e->getMessage() . "\n";
         }
+    }
+
+    public function getcomments($imagenu)
+    {
+        try{
+            $sql = 'SELECT * FROM comments WHERE imagenu = :imagenu';
+            $exe = $this->conns->prepare($sql);
+            $exe->bindParam(':imagenu', $imagenu);
+            $exe->execute();
+            $val = $exe->setFetchMode(PDO::FETCH_ASSOC);
+            return ($exe->fetchAll());
+        }catch (PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+    }
+
+    public function getlikes($imagenu)
+    {
+        try{
+            $sql = 'SELECT * FROM likes WHERE imagenu = :imagenu';
+            $exe = $this->conns->prepare($sql);
+            $exe->bindParam(':imagenu', $imagenu);
+            $exe->execute();
+            $val = $exe->setFetchMode(PDO::FETCH_ASSOC);
+            return ($exe->fetchAll());
+        }catch (PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+    }
+
+    public function emailcomment($userid)
+    {
+        try{
+            $sql = 'SELECT * FROM users WHERE userid = :userid';
+            $exe = $this->conns->prepare($sql);
+            $exe->bindParam(':userid', $userid);
+            $exe->execute();
+            $val = $exe->setFetchMode(PDO::FETCH_ASSOC);
+            $user  = $exe->fetchAll();
+        }catch (PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+        $to = $user[0]['email'];
+        $subject = "Someone commented on your image";
+
+        $message =  "the image commented on\n";
+        $headers = "Content-type: text\r\n";
+        mail($to,$subject, $message,$headers);
+
+    }
+    public function __destruct(){
+        $this->conns = NULL;
     }
 }
 ?>
--- publicgallery.php
+++ publicgallery.php
@@ -1,25 +1,32 @@
 <?php
     session_start();
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
     $retrive = array();
     $not_val = "";
     foreach($_POST as $key => $value)
         $retrive[$key] = $value;
     if ($_SESSION["username"]  && $retrive['submit'])
     {
-        if($retrive['comment'] && $retrive['userid'] && $_SESSION['username'])
+        if($retrive['comment'] && $retrive['userid'] && $_SESSION['username'] && $retrive['imagenu'])
         {
-            include('commentnlike.php');
+            include_once('commentnlike.php');
+            $email = $ad->getuserid2($_SESSION['username']);
             $ad = new commentnlike();
-            $ad->addcomment($retrive['comment'], $_SESSION['username'], $retrive['userid']);
+            $ad->addcomment($retrive['comment'], $_SESSION['username'], $retrive['userid'], $retrive['imagenu']);
+            $ad->emailcomment($_SESSION['username']);
+            unset($ad);
         }
-
     }elseif ($_SESSION["username"] && $retrive['like'])
     {
         if($retrive['like'] && $_SESSION['username'])
         {
-            include('commentnlike.php');
+            include_once('commentnlike.php');
             $ad = new commentnlike();
-            $ad->addlike($_SESSION['username'], $retrive['like']);
+            $ad->addlike($_SESSION['username'], $retrive['like'], $retrive['imagenu']);
+            $ad->emailcomment($_SESSION['username']);
+            unset($ad);
         }
     }
 ?>
@@ -42,6 +49,8 @@
     <div>
         <?php
             include_once('./picdb.php');
+            include_once('commentnlike.php');
+            $hold = new commentnlike();
             $arr = new picdb();
             $display = $arr->getall();
             $i = 0;
@@ -50,16 +59,30 @@
                 echo '<div style="width: 450px; hight: 450px; margin-left: 450px; margin-bottom: 30px;"><div><img src="'.$display[$i]['images'].'" style="width: 450px; hight: 450px;"></div>';
                 if($_SESSION['username'])
                 {
-                    echo '<div id="'.$display[$i]['timess'].'"><button id="'.$display[$i]['timess'].'" onclick="displays('.$display[$i]['num'].','.$display[$i]['timess'].' )">comment</button>';
-                    echo '<form action="publicgallery.php" method="post"><button id="'.$display[$i]['timess'].'" type="submit" name="like" value="'.$display[$i]['userid'].'">like</button></form></div><br/>';
+                    $lik = count($hold->getlikes($display[$i]['num']));
 
-                    echo '<div id="'.$display[$i]['num'].'" style="display:none;"><form action="publicgallery.php" method="post"><textarea name="comment"  id="'.$display[$i]['num'].'" rows="4" cols="50"></textarea>';
+                    $holds = $hold->getcomments($display[$i]['num']);
+                    echo '<div id="'.$display[$i]['timess'].'"><button id="'.$display[$i]['timess'].'" onclick="displays('.$display[$i]['num'].','.$display[$i]['timess'].' )">comment '.count($holds).'</button>';
+                    echo '<form action="publicgallery.php" method="post"><button id="'.$display[$i]['timess'].'" type="submit" name="like" value="'.$display[$i]['userid'].'">like '.$lik.'</button>';
+                    echo '<input type="hidden" name="imagenu" value="'.$display[$i]['num'].'"></form></div><br/>';
+
+                    echo '<div id="'.$display[$i]['num'].'" style="display:none;">';
+                    $j = 0;
+                    while($j < count($holds))
+                    {
+                       echo '<textarea rows="4" cols="50" disabled>'.$holds[$j]['comment'].'</textarea>';
+                       $j++;
+                    }
+                    echo '<form action="publicgallery.php" method="post"><textarea name="comment"  id="'.$display[$i]['num'].'" rows="4" cols="50"></textarea>';
                     echo '<input type="hidden" name="userid" value="'.$display[$i]['userid'].'">';
+                    echo '<input type="hidden" name="imagenu" value="'.$display[$i]['num'].'">';
                     echo '<button type="submit" value="comment" id="'.$display[$i]['num'].'" name="submit">comment</button></form></div>';
+                    unset($holds);
                 }
                 echo '</div>';
                 $i++;
-            } 
+            }
+            unset($hold); 
         ?>
     </div>
     <div>
